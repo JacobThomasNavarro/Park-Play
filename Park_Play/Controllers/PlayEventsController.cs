a8c529baddc39ad34d7b2b3c26849791447a1aba
Jake Navarro <jt10navarro@gmail.com>
2019-11-20 09:33:53 -0600

    added play event and skill sport user controllers

---
+++ Park_Play/Controllers/PlayEventsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNet.Identity;
+using Park_Play.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Park_Play.Controllers
+{
+    public class PlayEventsController : Controller
+    {
+        ApplicationDbContext context;
+
+        public PlayEventsController()
+        {
+            context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            return View(context.PlayEvents.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            PlayEvent playEvent = context.PlayEvents.Where(u => u.PlayEventId == id).FirstOrDefault();
+            return View(playEvent);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            PlayEvent playEvent = new PlayEvent();
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(PlayEvent playEvent)
+        {
+            try
+            {
+                context.PlayEvents.Add(playEvent);
+                context.SaveChanges();
+
+                string id = User.Identity.GetUserId();
+                var user = context.Users.Where(u => u.ApplicationId == id).FirstOrDefault();
+                playEvent.PlayEventId = user.UserId;
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            PlayEvent playEvent = context.PlayEvents.Where(u => u.PlayEventId == id).FirstOrDefault();
+            return View(playEvent);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, PlayEvent playEvent)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                PlayEvent editedPlayEvent = context.PlayEvents.Where(c => c.PlayEventId == id).FirstOrDefault();
+                editedPlayEvent.skillLevel = playEvent.skillLevel;
+                editedPlayEvent.StartTime = playEvent.StartTime;
+                editedPlayEvent.numberOfPlayers = playEvent.numberOfPlayers;
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            PlayEvent playEvent = context.PlayEvents.Where(u => u.PlayEventId == id).FirstOrDefault();
+            return View(playEvent);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, PlayEvent playEvent)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                PlayEvent playEventToDelete = context.PlayEvents.Where(u => u.PlayEventId == id).FirstOrDefault();
+                context.PlayEvents.Remove(playEventToDelete);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Park_Play/Controllers/SkillSportUsersController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNet.Identity;
+using Park_Play.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Park_Play.Controllers
+{
+    public class SkillSportUsersController : Controller
+    {
+        ApplicationDbContext context;
+
+        public SkillSportUsersController()
+        {
+            context = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            return View(context.SkillSportUsers.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            SkillSportUser skillSportUser = context.SkillSportUsers.Where(u => u.SkillSportUserId == id).FirstOrDefault();
+            return View(skillSportUser);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            SkillSportUser skillSportUser = new SkillSportUser();
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(SkillSportUser skillSportUser)
+        {
+            try
+            {
+                context.SkillSportUsers.Add(skillSportUser);
+                context.SaveChanges();
+
+                string id = User.Identity.GetUserId();
+                var user = context.Users.Where(u => u.ApplicationId == id).FirstOrDefault();
+                skillSportUser.SkillSportUserId = user.UserId;
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+           SkillSportUser skillSportUser = context.SkillSportUsers.Where(u => u.SkillSportUserId == id).FirstOrDefault();
+            return View(skillSportUser);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SkillSportUser skillSportUser)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                SkillSportUser editedSkillSportUser = context.SkillSportUsers.Where(c => c.SkillSportUserId == id).FirstOrDefault();
+                editedSkillSportUser.skillLevel = skillSportUser.skillLevel;
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            SkillSportUser skillSportUser = context.SkillSportUsers.Where(u => u.SkillSportUserId == id).FirstOrDefault();
+            return View(skillSportUser);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, SkillSportUser skillSportUser)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                SkillSportUser skillSportUserToDelete = context.SkillSportUsers.Where(u => u.SkillSportUserId == id).FirstOrDefault();
+                context.SkillSportUsers.Remove(skillSportUserToDelete);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
