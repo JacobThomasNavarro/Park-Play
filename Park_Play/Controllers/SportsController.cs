271b99edd8c4b77a09f84a6c009691880232436b
Jake Navarro <jt10navarro@gmail.com>
2019-11-20 09:51:34 -0600

    added sport controller

---
+++ Park_Play/Controllers/SportsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNet.Identity;
+using Park_Play.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Park_Play.Controllers
+{
+    public class SportsController : Controller
+    {
+        ApplicationDbContext context;
+
+        public SportsController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Sports
+        public ActionResult Index()
+        {
+            return View(context.Sports.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            Sport sport = context.Sports.Where(u => u.SportId == id).FirstOrDefault();
+            return View(sport);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            Sport sport = new Sport();
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(Sport sport)
+        {
+            try
+            {
+                context.Sports.Add(sport);
+                context.SaveChanges();
+
+                string id = User.Identity.GetUserId();
+                var user = context.Users.Where(u => u.ApplicationId == id).FirstOrDefault();
+                sport.SportId = user.UserId;
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Sport sport = context.Sports.Where(u => u.SportId == id).FirstOrDefault();
+            return View(sport);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Sport sport)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Sport editedSport = context.Sports.Where(c => c.SportId == id).FirstOrDefault();
+                editedSport.sportName = sport.sportName;
+                context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Sport sport = context.Sports.Where(u => u.SportId == id).FirstOrDefault();
+            return View(sport);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Sport sport)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Sport sportToDelete = context.Sports.Where(u => u.SportId == id).FirstOrDefault();
+                context.Sports.Remove(sportToDelete);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
